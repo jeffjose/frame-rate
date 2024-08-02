3a58d4d4ebc41687a0a4eb6044c6374c6f9a5a58
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-08-02 15:47:08 -0700

    WORKS: print ffmpeg command

--- src/frame_rate/__init__.py
+++ src/frame_rate/__init__.py
@@ -1,5 +1,3 @@
-import PIL
-import contextlib
 from PIL import Image, ImageDraw
 import typer
 from tailwind_colors import TAILWIND_COLORS_HEX
@@ -19,7 +17,7 @@ def hello(name: str) -> int:
 
 
 @app.command()
-def create(input: str = typer.Option(), output:str = typer.Option(), fps: int = typer.Option(), duration: int = typer.Option()) -> int:
+def create(width:int = 800, height: int = 600, input: str = typer.Option(), output:str = 'output.mp4', fps: int = typer.Option(), duration: int = typer.Option()) -> int:
 
     num_frames = fps * duration 
     frame_time = round(1000 / fps, 2)
@@ -29,23 +27,23 @@ def create(input: str = typer.Option(), output:str = typer.Option(), fps: int = 
     print(f"  total duration: {duration}s")
     print(f"total num_frames: {num_frames}")
 
-
     frames = []
     x, y = 0, 100
     for i in range(num_frames):
-        new_frame = create_image_with_ball(800, 600, x, y, 40)
+        new_frame = create_image_with_ball(height, height, x, y, 40)
         frames.append(new_frame)
         x += 1
         #y += 40
 
-        filename = f'ball/ball-{str(i).zfill(10)}.jpg'
+        filename = f'ball/ball-{str(i).zfill(10)}.png'
 
         new_frame.save(filename)
 
+    ffmpeg_cmd = f"ffmpeg -framerate {fps} -pattern_type glob -i 'ball/ball*png' {output}"
+    print(ffmpeg_cmd)
+
     # Save into a GIF file that loops forever
     #frames[0].save('moving_ball.gif', format='GIF', optimize=False, append_images=frames[1:], save_all=True, duration=frame_time, loop=0)
-
-
     return 0
 
 def main():
